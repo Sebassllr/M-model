c8b41ce097bbf43d0b7ad14fa05d39efa4a27b88
pol_sebastianluna <sebastianluna@process-on-line.com>
2019-03-05 21:26:38 -0500

    Se agregan dependencias y conexión con la db

--- src/containers/AddModel/addModel.js
+++ src/containers/AddModel/addModel.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import Input from "../../components/Input/input"
 import classes from './addModel.module.css';
-import Title from '../../components/Title/title'
+import Title from '../../components/Title/title';
+import axios from 'axios';
 
 class AddModel extends Component{
 
     state = {
         inputs: {
             name: '',
-            description: ''
+            description: '',
+            activities: [],
+        },
+        activity: {
+            name: '',
+            description: '',
         }
     }
 
@@ -23,33 +29,61 @@ class AddModel extends Component{
             case 2:
                 properties.inputs.description = event.target.value;
                 break;
+            case 3:
+                properties.activity.name = event.target.value;
+                break;
+            case 4:
+                properties.activity.description = event.target.value;
+                break;
         }
         this.setState({...properties});
     }
 
-    render(){
+    addActivity = () => { 
+        const props = {...this.state};
+        const activity = {
+            ...props.activity
+        };
+        const list = props.inputs.activities;
+        list.push(activity);
+        props.inputs.activities = list;
+        props.activity.name = '';
+        props.activity.description = '';
+        this.setState(props);
+    }
 
+    saveModel = () => {
+        axios.post('saveModel', this.state.inputs, { crossdomain: true }).then(response =>{
+            this.props.closeModal();
+        })
+    }
+
+    render(){
+        const styles = {
+            height: '282px'
+        };
         return(
             <>
                 <form>
                     <div className="displayFlex">
                         <div className={classes.widthLeft}>
-                            <Input value={this.state.name} onChange={(event) => {this.inputHandler(event, 1)}} name="Nombre" type="input" required/>
-                            <Input value={this.state.description} onChange={(event) => {this.inputHandler(event, 2)}} name="Descripción" type="textarea"/>
+                            <Input value={this.state.inputs.name} onChange={(event) => {this.inputHandler(event, 1)}} name="Nombre" type="input" required/>
+                            <Input style={styles} value={this.state.description} onChange={(event) => {this.inputHandler(event, 2)}} name="Descripción" type="textarea"/>
                         </div>
                         <div className={[classes.widthRigth, classes.border].join(" ")}>
                             <Title title="Actividades"/>
                             <div className={classes.paddingInputs}>
-                                <Input value={this.state.name} onChange={(event) => {this.inputHandler(event, 1)}} name="Nombre" type="input" required/>
+                                <Input value={this.state.activity.name} onChange={(event) => {this.inputHandler(event, 3)}} name="Nombre" type="input" required/>
+                                <Input value={this.state.activity.description} onChange={(event) => {this.inputHandler(event, 4)}} name="Descripción" type="textarea"/>
+                                <button type="button" onClick={this.addActivity}>Agregar</button>
                             </div>
                         </div>
                     </div>
                 </form>
-                <button type="button" onClick={this.props.closeModal}>Cerrar!</button>
+                <button type="button" onClick={this.saveModel}>Guardar!</button>
             </>
         )
     }
-
 }
 
 export default AddModel;
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import MainView from '../containers/MainView/mainView';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'localhost:3001/';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN'; 
+axios.defaults.headers.post['Content-type'] = 'application/json';
 
 class App extends Component {
   render() {
