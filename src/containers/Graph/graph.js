d905902a18e155c9ce5ef97cfd9981d0b317479f
valentinalenis <valentinalenis@hotmail.com>
2019-04-06 22:23:09 -0500

    Graph ok

--- src/containers/Graph/graph.js
+++ src/containers/Graph/graph.js
@@ -1,12 +1,59 @@
 import React, { Component } from 'react';
+import BpmnModeler from 'bpmn-js/lib/Modeler';
+import 'bpmn-js/dist/assets/diagram-js.css';
+import 'bpmn-font/dist/css/bpmn-embedded.css';
+import BpmnViewer from 'bpmn-js';
+import classes from './graph.module.css';
+import { emptyBpmn } from  '../empty.bpmn';
+import propertiesPanelModule from 'bpmn-js-properties-panel';
+import propertiesProviderModule from 'bpmn-js-properties-panel/lib/provider/camunda';
+import camundaModdleDescriptor from 'camunda-bpmn-moddle/resources/camunda';
 
 class Graph extends Component{
-    render(){
+    modeler = null;
+    
+    componentDidMount = () => {
+        this.modeler = new BpmnModeler({
+            container: '#bpmnview',
+            keyboard: {
+                bindTo: window
+            },
+            
+            additionalModules: [
+                propertiesPanelModule,
+                propertiesProviderModule
+            ],
+            moddleExtensions: {
+                camunda: camundaModdleDescriptor
+            }
+        });
+
+        this.newBpmnDiagram();
+    }
+
+    newBpmnDiagram = () => {
+        this.openBpmnDiagram(emptyBpmn);
+    }
+
+    openBpmnDiagram = (xml) => {
+        this.modeler.importXML(xml, (error) => {
+            if (error) {
+                return console.log('fail import xml');
+            }
+
+            var canvas = this.modeler.get('canvas');
+
+            canvas.zoom('fit-viewport');
+        });
+    }
+
+    render = () => {
         return(
-            <div>
-                Acá va el graficador :)
+            <div id="bpmncontainer">
+                <div id="propview" style={{ width: '0%', height: '98vh', float: 'right', maxHeight: '98vh', overflowX: 'auto' }}></div>
+                <div id="bpmnview" style={{ width: '100%', height: '98vh', float: 'left',classes  }}></div>
             </div>
-        );
+        )
     }
 }
 
